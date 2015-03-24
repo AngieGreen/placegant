9a0bf506f53e2b6b1c11674ef5150e3bf1caf7a8
Angie Green <angie@greenorleans.com>
2015-03-24 10:41:41 -0500

    Update tests

--- test/placegant_test.rb
+++ test/placegant_test.rb
@@ -2,11 +2,6 @@ require 'placegant'
 require 'test/unit'
 
 class GantTest < Test::Unit::TestCase
-  def test_no_params
-    image = PlaceGant.image
-    assert_equal "http://placegant.me/pg/#{PlaceGant::DEFAULT_WIDTH}/#{PlaceGant::DEFAULT_HEIGHT}", image
-  end
-
   def test_width_height
     image = PlaceGant.image(300, 200)
     assert_equal "http://placegant.me/pg/300/200", image
